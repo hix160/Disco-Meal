b5b82c4d212559c8de3e007bbf72220877d54324
hi160 <janciskauls@gmail.com>
2025-01-10 21:14:50 +0200

    establish connection for backend and db

--- backend/db/pool.js
+++ backend/db/pool.js
@@ -8,4 +8,8 @@ module.exports = new Pool({
     database: process.env.PGDATABASE,
     password: process.env.PGPASSWORD,
     port: process.env.PGPORT || 5432, //default postgresql port is 5432
+    ssl: {
+        // Enable SSL connection
+        rejectUnauthorized: false,
+      },
 })
